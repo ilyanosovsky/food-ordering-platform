3a84247abd0f5c789a265f2f672f2c2327cd935d
ilyanosovsky <imnosovsky@gmail.com>
2024-05-31 14:54:41 +0300

    nav bars

--- client/src/components/MainNav.tsx
+++ client/src/components/MainNav.tsx
@@ -1,11 +1,16 @@
-
+import { Button } from "./ui/button";
 
 const MainNav = () => {
   return (
-    <div>
-      Main
-    </div>
-  )
-}
+    <span className="flex space-x-2 items-center">
+      <Button
+        variant="ghost"
+        className="font-bold hover:text-orange-500 hover:bg-white"
+      >
+        Log In
+      </Button>
+    </span>
+  );
+};
 
-export default MainNav
+export default MainNav;
--- client/src/components/MobileNav.tsx
+++ client/src/components/MobileNav.tsx
@@ -1,10 +1,31 @@
+import { Menu } from "lucide-react";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetTitle,
+  SheetTrigger,
+} from "./ui/sheet";
+import { Separator } from "./ui/separator";
+import { Button } from "./ui/button";
 
 const MobileNav = () => {
   return (
-    <div>
-        Mobile
-    </div>
-  )
-}
+    <Sheet>
+      <SheetTrigger>
+        <Menu className="text-orange-500" />
+      </SheetTrigger>
+      <SheetContent className="space-y-3">
+        <SheetTitle>
+          <span> Welcome to OrderEats.com!</span>
+        </SheetTitle>
+        <Separator />
+        <SheetDescription className="flex flex-col gap-4">
+          <Button className="flex-1 font-bold bg-orange-500">Log In</Button>
+        </SheetDescription>
+      </SheetContent>
+    </Sheet>
+  );
+};
 
-export default MobileNav
+export default MobileNav;
