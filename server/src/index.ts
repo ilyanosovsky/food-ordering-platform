dce3042e76c3c87ec69c7994ac8db76dcfa9389d
ilyanosovsky <imnosovsky@gmail.com>
2024-06-01 15:15:43 +0300

    create user endpoint

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
+import myUserRoute from "./routes/MyUserRoute";
 
 mongoose
   .connect(process.env.MONGODB_CONNECTION_STRING as string)
@@ -11,9 +12,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/test", async (req: Request, res: Response) => {
-  res.json({ message: "hello!" });
-});
+app.use("/api/my/user", myUserRoute);
 
 app.listen(5001, () => {
   console.log("server started on localhost: 5001");
