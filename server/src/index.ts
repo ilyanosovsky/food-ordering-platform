f9fe450ea867d2d1156f8e24cc0635201b4e4895
ilyanosovsky <imnosovsky@gmail.com>
2024-06-04 22:42:26 +0300

    deployment set up

--- server/src/index.ts
+++ server/src/index.ts
@@ -12,6 +12,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/health", async (req: Request, res: Response) => {
+  res.send({ message: "health OK!" });
+});
+
 app.use("/api/my/user", myUserRoute);
 
 app.listen(5001, () => {
