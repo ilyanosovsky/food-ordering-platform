1c13a4f12c234af64d326a2be9f315f68d5f1fae
ilyanosovsky <imnosovsky@gmail.com>
2024-06-07 22:02:07 +0300

    update rest API

--- server/src/routes/MyRestaurantRoute.ts
+++ server/src/routes/MyRestaurantRoute.ts
@@ -23,5 +23,13 @@ router.post(
   jwtParse,
   MyRestaurantController.createMyRestaurant
 );
+router.put(
+  "/",
+  upload.single("imageFile"),
+  validateMyRestaurantRequest,
+  jwtCheck,
+  jwtParse,
+  MyRestaurantController.updateMyRestaurant
+);
 
 export default router;
