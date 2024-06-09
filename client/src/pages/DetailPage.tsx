dd11fa31adeb039320a66f1245c497bfddf20561
ilyanosovsky <imnosovsky@gmail.com>
2024-06-09 23:14:20 +0300

    add checkout button

--- client/src/pages/DetailPage.tsx
+++ client/src/pages/DetailPage.tsx
@@ -7,8 +7,8 @@ import { Card, CardFooter } from "@/components/ui/card";
 import { useState } from "react";
 import { useParams } from "react-router-dom";
 import { MenuItem as MenuItemType } from "../types";
-// import CheckoutButton from "@/components/CheckoutButton";
-// import { UserFormData } from "@/forms/user-profile-form/UserProfileForm";
+import CheckoutButton from "@/components/CheckoutButton";
+import { UserFormData } from "@/forms/user-profile-form/UserProfileForm";
 // import { useCreateCheckoutSession } from "@/api/OrderApi";
 
 export type CartItem = {
@@ -79,30 +79,32 @@ const DetailPage = () => {
     });
   };
 
-//   const onCheckout = async (userFormData: UserFormData) => {
-//     if (!restaurant) {
-//       return;
-//     }
+  const onCheckout = async (userFormData: UserFormData) => {
+    console.log("userFormData", userFormData);
+    // if (!restaurant) {
+    //   return;
+    // }
 
-//     const checkoutData = {
-//       cartItems: cartItems.map((cartItem) => ({
-//         menuItemId: cartItem._id,
-//         name: cartItem.name,
-//         quantity: cartItem.quantity.toString(),
-//       })),
-//       restaurantId: restaurant._id,
-//       deliveryDetails: {
-//         name: userFormData.name,
-//         addressLine1: userFormData.addressLine1,
-//         city: userFormData.city,
-//         country: userFormData.country,
-//         email: userFormData.email as string,
-//       },
-//     };
+    // const checkoutData = {
+         
+    //   cartItems: cartItems.map((cartItem) => ({
+    //     menuItemId: cartItem._id,
+    //     name: cartItem.name,
+    //     quantity: cartItem.quantity.toString(),
+    //   })),
+    //   restaurantId: restaurant._id,
+    //   deliveryDetails: {
+    //     name: userFormData.name,
+    //     addressLine1: userFormData.addressLine1,
+    //     city: userFormData.city,
+    //     country: userFormData.country,
+    //     email: userFormData.email as string,
+    //   },
+    // };
 
-//     const data = await createCheckoutSession(checkoutData);
-//     window.location.href = data.url;
-//   };
+    // const data = await createCheckoutSession(checkoutData);
+    // window.location.href = data.url;
+  };
 
   if (isLoading || !restaurant) {
     return "Loading...";
@@ -136,11 +138,11 @@ const DetailPage = () => {
               removeFromCart={removeFromCart}
             />
             <CardFooter>
-              {/* <CheckoutButton
+              <CheckoutButton
                 disabled={cartItems.length === 0}
                 onCheckout={onCheckout}
-                isLoading={isCheckoutLoading}
-              /> */}
+                // isLoading={isCheckoutLoading}
+              />
             </CardFooter>
           </Card>
         </div>
