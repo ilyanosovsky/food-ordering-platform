78226437e62d3b3a534784f2d2695f4f62778883
ilyanosovsky <imnosovsky@gmail.com>
2024-06-09 23:30:15 +0300

    add user picture

--- client/src/components/UsernameMenu.tsx
+++ client/src/components/UsernameMenu.tsx
@@ -1,4 +1,4 @@
-import { CircleUserRound } from "lucide-react";
+// import { CircleUserRound } from "lucide-react";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -9,15 +9,31 @@ import { useAuth0 } from "@auth0/auth0-react";
 import { Link } from "react-router-dom";
 import { Separator } from "./ui/separator";
 import { Button } from "./ui/button";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 
 const UsernameMenu = () => {
   const { user, logout } = useAuth0();
 
+  // Function to extract initials from the user's name or email
+  const getInitials = (name: string) => {
+    const nameParts = name.split(" ");
+    if (nameParts.length > 1) {
+      return nameParts[0][0] + nameParts[1][0];
+    }
+    return name.substring(0, 2);
+  };
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger className="flex items-center px-3 font-bold hover:text-orange-500 gap-2">
-        <CircleUserRound className="text-orange-500" />
-        {user?.email}
+        <Avatar>
+          <AvatarImage src={user?.picture || ""} alt={user?.name || "User"} />
+          <AvatarFallback>
+            {user ? getInitials(user.name || user.email || "") : "?"}
+          </AvatarFallback>
+        </Avatar>
+        {/* <CircleUserRound className="text-orange-500" /> */}
+        {/* {user?.email} */}
       </DropdownMenuTrigger>
       <DropdownMenuContent>
         <DropdownMenuItem>
