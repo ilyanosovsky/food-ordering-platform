bc1dee9719f6b1a18f43a31ee08557f0b2853b15
ilyanosovsky <imnosovsky@gmail.com>
2024-06-17 23:53:33 +0300

    menu items images updated

--- client/src/components/Search/MenuItem.tsx
+++ client/src/components/Search/MenuItem.tsx
@@ -1,5 +1,6 @@
 import { MenuItem as MenuItemType } from "@/types";
 import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
+import { AspectRatio } from "../ui/aspect-ratio";
 
 type Props = {
   menuItem: MenuItemType;
@@ -8,13 +9,25 @@ type Props = {
 
 const MenuItem = ({ menuItem, addToCart }: Props) => {
   return (
-    <Card className="cursor-pointer" onClick={addToCart}>
-      <CardHeader>
-        <CardTitle>{menuItem.name}</CardTitle>
-      </CardHeader>
-      <CardContent className="font-bold">
-        ${(menuItem.price / 100).toFixed(2)}
-      </CardContent>
+    <Card className="flex  cursor-pointer" onClick={addToCart}>
+      <AspectRatio ratio={16 / 3} className="flex ">
+        <img
+          src={
+            menuItem.imageUrl ||
+            "https://res.cloudinary.com/dueuo2k5y/image/upload/v1718559813/yeqo42dez4bzkejpitfi.png"
+          } 
+          alt={menuItem.name}
+          className="rounded-md object-cover"
+        />
+      </AspectRatio>
+      <div className="flex flex-col justify-center  md:mr-10">
+        <CardHeader>
+          <CardTitle>{menuItem.name}</CardTitle>
+        </CardHeader>
+        <CardContent className="font-bold">
+          ${(menuItem.price / 100).toFixed(2)}
+        </CardContent>
+      </div>
     </Card>
   );
 };
